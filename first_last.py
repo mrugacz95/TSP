ac38d9f8da4cca1409533cadf5c2bd97bd8b2a12
Marcin Mrugas <marcin.mrugas@snow.dog>
2018-11-07 09:27:05 +0100

    First last refactor

--- first_last.py
+++ first_last.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 import matplotlib
-from matplotlib import pyplot as plt
-from subprocess import check_output
 import numpy as np
+from matplotlib import pyplot as plt
 
-#plt.rc('text', usetex=True)
-#plt.rc('font', family='Arial')
-matplotlib.rc('font', **{'sans-serif' : 'Arial',
-                             'family' : 'sans-serif'})
+matplotlib.rc('font', **{'sans-serif': 'Arial',
+                         'family': 'sans-serif'})
+
 
 def getPlotStuff(alg):
     if alg == '7':
@@ -16,39 +14,43 @@ def getPlotStuff(alg):
         return ("LS Steepest", 1)
     return ()
 
-#with open('first_last_finest', 'r') as fh:
-with open('first_last_small', 'r') as fh:
-    a = fh.read()
-lines = a.split("\n")[:-1]
 
-instances = {
-            }
+def main():
+    filename = 'first_last_small'
+    with open(filename, 'r') as fh:
+        a = fh.read()
+    lines = a.split("\n")[:-1]
 
-labels = []
-for i in range(0, len(lines), 4):
-    print(lines[i])
-    plot_stuff = getPlotStuff(lines[i].split()[-1])
-    first_s = np.fromstring(lines[i+2], dtype=np.float32, sep=' ')
-    last_s = np.fromstring(lines[i+3], dtype=np.int32, sep=' ')
+    instances = {
+    }
 
-    instances[lines[i]] = {}
-    instances[lines[i]]['first_s'] = first_s
-    instances[lines[i]]['last_s'] = last_s
-    instances[lines[i]]['color'] = np.random.rand(1, 3)
-    instances[lines[i]]['name'] = lines[i].split()[0].replace(".atsp", "") + " " + plot_stuff[0]
-    if plot_stuff[1]:
-        print("XDD")
-        instances[lines[i]]['type'] = 'x'
-    else:
-        instances[lines[i]]['type'] = 'o'
+    for i in range(0, len(lines), 4):
+        print(lines[i])
+        plot_stuff = getPlotStuff(lines[i].split()[-1])
+        first_s = np.fromstring(lines[i + 2], dtype=np.float32, sep=' ')
+        last_s = np.fromstring(lines[i + 3], dtype=np.int32, sep=' ')
 
-# wykres 1
-for k, v in instances.items():
-    print(v)
-    plt.scatter(v['first_s'], v['last_s'], c=v['color'], s=1.5, marker='^',#v['type'],
-                label=v['name'])
-plt.legend()
-plt.xlabel("długość rozwiązania początkowego")
-plt.ylabel("długość rozwiązania końcowego")
-plt.show()
+        instances[lines[i]] = {}
+        instances[lines[i]]['first_s'] = first_s
+        instances[lines[i]]['last_s'] = last_s
+        instances[lines[i]]['color'] = np.random.rand(1, 3)
+        instances[lines[i]]['name'] = lines[i].split()[0].replace(".atsp", "") + " " + plot_stuff[0]
+        if plot_stuff[1]:
+            print("XDD")
+            instances[lines[i]]['type'] = 'x'
+        else:
+            instances[lines[i]]['type'] = 'o'
 
+    # plot 3
+    for k, v in instances.items():
+        print(v)
+        plt.scatter(v['first_s'], v['last_s'], c=v['color'], s=1.5, marker='^',  # v['type'],
+                    label=v['name'])
+    plt.legend()
+    plt.xlabel("długość rozwiązania początkowego")
+    plt.ylabel("długość rozwiązania końcowego")
+    plt.savefig(f'report/pics/{filename}.pdf')
+
+
+if __name__ == '__main__':
+    main()
